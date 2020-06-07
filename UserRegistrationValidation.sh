8dc410001eaee692e1c84dfb47ed94a57b6d5096
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-07 14:22:29 +0530

    Added: email id validated

--- UserRegistrationValidation.sh
+++ UserRegistrationValidation.sh
@@ -5,13 +5,14 @@ echo -e "WELCOME TO USER REGISTRATION VALIDATION PROBLEM\n\n"
 
 #########REQUIRED VARIABLES##############
 namePattern="[[:upper:]][a-zA-Z]{2,}$"
-flag=0
-
+emailPattern="^(abc)([a-zA-Z0-9\_\-\.\+]*)@(bl)\.(co)\.*([a-zA-Z]{2})*"
 
 ###########FUNCTIONS################
 function CheckNameFirstLetter
 {
   nameUser=$1
+  flag=0
+
   if [[ $nameUser =~ $namePattern ]];then
       flag=1
    else
@@ -22,21 +23,19 @@ function CheckNameFirstLetter
 function CheckName
 {
  nameUser=$1
- 
+
  #Checking user first name of User
  CheckNameFirstLetter $nameUser
  if [[ $flag -eq 1 ]];then
-   echo -e "\nValid $nameUser"
+   echo "Valid Name: $nameUser"
  else
-   echo -e "\nInvalid $nameUser"
+   echo "Invalid $nameUser"
    echo "First character must be capital and lenght must be more than 2 characters."
    echo "Convert first letter to uppercase ... Checking again"
    nameUser=`echo $nameUser | sed -e "s/\b\(.\)/\u\1/g"`
    CheckNameFirstLetter $nameUser
-   if [[ $flag -eq 1 ]];then
-       echo -e "\nValid $nameUser"
-   else
-       echo -e "\nInvalid $nameUser"
+   if [[ $flag -ne 1 ]];then
+       echo "Invalid $nameUser"
        echo "First character must be capital and lenght must be more than 2 characters."
        echo "Converion failed"
        echo "Execution terminating ... Try again ..."
@@ -45,9 +44,24 @@ function CheckName
 
 }
 
+function CheckEmail
+{
+ emailUser=$1
+
+ if [[ $emailUser =~ $emailPattern ]];then
+    echo "Valid Email Id."
+ else
+    echo "Invalid Email Id"
+    echo "Execution Terminated ... Try again"
+    exit 0
+ fi
+}
+
+
 #Taking Input from user
 read -p "Enter User First Name: " fname
 read -p "Enter User Last Name: " lname
+read -p "Enter User Email Id:" emailId
 
 #Checking user first name of User
 CheckName $fname
@@ -57,10 +71,13 @@ fname=$nameUser
 CheckName $lname
 lname=$nameUser
 
+#Checking user's email id
+CheckEmail $emailId
 
 echo -e "\n\n---------------------------------------------"
 echo "USER INFORMATION"
 echo "---------------------------------------------"
 echo "First Name: $fname"
-echo "Lst Name: $lname"
+echo "Last Name: $lname"
+echo "Email Id: $emailId"
 echo "---------------------------------------------"
