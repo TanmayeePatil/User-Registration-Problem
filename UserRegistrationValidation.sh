b48f919bf03ad3f96994988b3b8d8131bdf7d44e
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-07 13:52:25 +0530

    Added: Last name validated

--- UserRegistrationValidation.sh
+++ UserRegistrationValidation.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 shopt -s extglob
 
-echo "WELCOME TO USER REGISTRATION VALIDATION PROBLEM"
+echo -e "WELCOME TO USER REGISTRATION VALIDATION PROBLEM\n\n"
 
-
+#########REQUIRED VARIABLES##############
 namePattern="[[:upper:]][a-zA-Z]{2,}$"
 flag=0
 
-read -p "Enter User First Name:" fname
 
+###########FUNCTIONS################
 function CheckNameFirstLetter
 {
-  if [[ $fname =~ $namePattern ]];then
+  nameUser=$1
+  if [[ $nameUser =~ $namePattern ]];then
       flag=1
    else
       flag=0
   fi
 }
 
-#Checking user first name of User
-CheckNameFirstLetter
-if [[ $flag -eq 1 ]];then
-   echo -e "\nValid $fname"
-else
-   echo -e "\nInvalid $fname"
+function CheckName
+{
+ nameUser=$1
+ 
+ #Checking user first name of User
+ CheckNameFirstLetter $nameUser
+ if [[ $flag -eq 1 ]];then
+   echo -e "\nValid $nameUser"
+ else
+   echo -e "\nInvalid $nameUser"
    echo "First character must be capital and lenght must be more than 2 characters."
    echo "Convert first letter to uppercase ... Checking again"
-   fname=`echo $fname | sed -e "s/\b\(.\)/\u\1/g"`
-   CheckNameFirstLetter
+   nameUser=`echo $nameUser | sed -e "s/\b\(.\)/\u\1/g"`
+   CheckNameFirstLetter $nameUser
    if [[ $flag -eq 1 ]];then
-       echo -e "\nValid $fname"
+       echo -e "\nValid $nameUser"
    else
-       echo -e "\nInvalid $fname"
+       echo -e "\nInvalid $nameUser"
        echo "First character must be capital and lenght must be more than 2 characters."
        echo "Converion failed"
        echo "Execution terminating ... Try again ..."
    fi
-fi
+ fi
+
+}
+
+#Taking Input from user
+read -p "Enter User First Name: " fname
+read -p "Enter User Last Name: " lname
+
+#Checking user first name of User
+CheckName $fname
+fname=$nameUser
+
+#Checking user lst name of User
+CheckName $lname
+lname=$nameUser
+
 
 echo -e "\n\n---------------------------------------------"
 echo "USER INFORMATION"
 echo "---------------------------------------------"
 echo "First Name: $fname"
+echo "Lst Name: $lname"
 echo "---------------------------------------------"
